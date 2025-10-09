f8df20844caa863dcca26d4cc44b41bdda446f7f
Rafael <jucatagalogcoc-wq>
2025-10-09 13:10:55 +0800

    justin

---
+++ NiceAdmin/modules/registration_form.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registration Form</title>
+</head>
+<body>
+  <h2>Registration Form</h2>
+
+  <form action="#" method="POST">
+    <label for="first_name">First Name</label>
+    <input type="text" id="first_name" name="first_name" placeholder="First Name" required>
+    <br><br>
+
+    <label for="last_name">Last Name</label>
+    <input type="text" id="last_name" name="last_name" placeholder="Last Name" required>
+    <br><br>
+
+    <label for="email">Email Address</label>
+    <input type="email" id="email" name="email" placeholder="Email Address" required>
+    <br><br>
+
+    <label for="address1">Address Line 1</label>
+    <input type="text" id="address1" name="address1" placeholder="Address Line 1" required>
+    <br><br>
+
+    <label for="address2">Address Line 2</label>
+    <input type="text" id="address2" name="address2" placeholder="Address Line 2">
+    <br><br>
+
+    <label for="city">City</label>
+    <input type="text" id="city" name="city" placeholder="City" required>
+    <br><br>
+
+    <label for="state">State</label>
+    <input type="text" id="state" name="state" placeholder="State" required>
+    <br><br>
+
+    <label for="zip">Zip Code</label>
+    <input type="text" id="zip" name="zip" placeholder="Zip Code" required>
+    <br><br>
+
+    <input type="submit" value="Submit">
+  </form>
+</body>
+</html>
